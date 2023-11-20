3838a1c3756d5c58366cecc8bdb2df9ab34988e0
lamideolaniyan <olaniyanolamide@gmail.com>
2023-11-20 10:53:41 +0100

    functions to get user selected ecll, store in DB and render selection

--- app.js
+++ app.js
@@ -5,10 +5,11 @@
  *
  */
 
-let marker1 = 'X';
-let marker2 = '';
+let marker1;
+let marker2;
 let player1 = '';
 let player2 = 'AI';
+const playerArr = [];
 
 const DOM = {
 	markerSelectorInput: document.querySelector('.marker__input'),
@@ -17,10 +18,12 @@ const DOM = {
 	gameBoard: document.querySelector('.container'),
 	startPage: document.querySelector('.game'),
 	cellArr: Array.from(document.querySelectorAll('.cell')),
+	newGameBtn: Array.from(document.querySelectorAll('.newBtn')),
 };
 const startGame = () => {
 	// GET PLAYER 1 MARKER
 	marker1 = DOM.markerSelectorInput.value;
+	marker1 ? marker1 : (marker1 = 'X');
 	// SET PLAYER MARKER
 	marker1 === 'X' ? (marker2 = 'O') : (marker2 = 'X');
 	// GET PLAYER MONICKER
@@ -40,8 +43,28 @@ const startGame = () => {
 	});
 };
 
+const getCell = (cell) => {
+	const selectedCell = +cell.dataset.id;
+	playerArr.push(selectedCell);
+
+	console.log(playerArr);
+};
+
+const displayMarker = (cell) => {
+	cell.textContent = marker1;
+};
+const newGame = () => {};
+
+// EVENT LISTENERS
 // DOM.startGameBtn.addEventListener('click', startGame);
 DOM.startPage.addEventListener('submit', (e) => {
 	startGame();
 	e.preventDefault();
 });
+
+DOM.cellArr.forEach((cell) =>
+	cell.addEventListener('click', (e) => {
+		getCell(e.target);
+		displayMarker(e.target);
+	})
+);
